8c49a0a19ea2016f2aa9c02ff5c61eebede5f8b8
Matthew <12hughesm@gmail.com>
2023-12-01 23:54:53 +0000

    Removed hardcoded wizard HTML and created a function to add it to the board

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,26 @@ import "./main.scss";
     DOM to get game board
     function to populate the board with x goblins
     will update in the future to create goblin objects
-    and place them on the board based on the x and y attributes */
+    and place them on the board based on the x and y attributes 
+    */
 
-const gameBoard = document.querySelector(".game-board")
-if(!gameBoard) throw new Error("Query error");
+const gameBoard = document.querySelector(".game-board");
+if (!gameBoard) throw new Error("Query error");
 
-const populateBoardWithGoblins = (numberOfGoblins: number) =>{
-    for (let i = 1; i <= numberOfGoblins; i++) {
-        gameBoard.innerHTML += `<img class="game-board__goblin game-board--goblin-${i}" src="./images/goblin.png" alt="">`
-    }
-}
+const getGoblinHTML = (goblinIdNumber: number) => {
+  return;
+  `<img class="game-board__goblin game-board--goblin-${goblinIdNumber}" src="./images/goblin.png" alt="">`;
+};
+
+const populateBoardWithGoblins = (numberOfGoblins: number) => {
+  for (let i = 1; i <= numberOfGoblins; i++) {
+    gameBoard.innerHTML += getGoblinHTML(i);
+  }
+};
+
+const addWizardToBoard = () => {
+  gameBoard.innerHTML += `<img class="game-board__player-character" src="./images/wizard.png" alt="">`;
+};
 
 populateBoardWithGoblins(10);
+addWizardToBoard();
