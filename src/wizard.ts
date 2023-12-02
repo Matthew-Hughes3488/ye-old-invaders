2fc0473ef829eb53ed7a678e9ccc2338af11d8c9
Matthew <12hughesm@gmail.com>
2023-12-02 14:51:47 +0000

    Updated wizard img link

--- src/wizard.ts
+++ src/wizard.ts
@@ -4,7 +4,7 @@ class Wizard{
 
     public getWizardHTML(){
         const wizardElement = document.createElement("img");
-        wizardElement.src = "./src/images/goblin.png"
+        wizardElement.src = "./src/images/wizard.png"
         wizardElement.classList.add("game-board__wizard");
         wizardElement.style.gridColumn = `${this.xCordinate} / span 1`;
         wizardElement.style.gridRow = `${this.yCordinate} / span 1`;
