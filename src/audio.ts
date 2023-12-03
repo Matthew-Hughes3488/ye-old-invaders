ff61a299f0d4e902c237884218edc127015e25c5
Matthew <12hughesm@gmail.com>
2023-12-03 16:17:04 +0000

    Implemented a constructor and createAudioHTML method

--- src/audio.ts
+++ src/audio.ts
@@ -2,11 +2,20 @@ class GameAudio {
   private audioElement: HTMLAudioElement;
   private audioIntervalId?: number;
 
-  constructor() {
-    
+  constructor(audioSrc : string) {
+    this.audioElement = this.createAudioHTML(audioSrc);
+    this.addToHTML();
   }
 
-  private createAudioHTML() {}
+  private createAudioHTML(audioSrc : string) : HTMLAudioElement {
+    const audioElement = document.createElement("audio");
+    audioElement.src = audioSrc;
+    return audioElement;
+  }
+
+  private addToHTML() {
+    document.body.appendChild(this.audioElement);
+  }
 
   public playAudio(intervalNumber: number = 0) {}
 
