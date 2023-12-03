4588ba484d780b0383e3de1c7440555a086cab6f
Matthew <12hughesm@gmail.com>
2023-12-03 16:11:28 +0000

    Added gameAudio class with methods that have not yet been implemented

---
+++ src/audio.ts
@@ -0,0 +1,20 @@
+class GameAudio {
+  private audioElement: HTMLAudioElement;
+  private audioIntervalId?: number;
+
+  constructor() {
+    
+  }
+
+  private createAudioHTML() {}
+
+  public playAudio(intervalNumber: number = 0) {}
+
+  public stopAudio() {}
+
+  setAudioVolume(volume: number) {}
+
+  private startInterval(intervalNumber: number) {}
+
+  private stopInterval() {}
+}
