e1f7d8e26c1b3f32ed7d9d1357e15ec907238255
Matthew <12hughesm@gmail.com>
2023-12-05 17:01:57 +0000

    changed the layout of main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import "./main.scss";
 import Game from "./game";
 
-const startButton = document.querySelector(".start-button");
-if (!startButton) throw new Error("ssd");
-
 let gameStarted = false;
 const game = new Game();
+
+const startButton = document.querySelector(".start-button");
+if (!startButton) throw new Error("Query Error");
+
 
 const handleStartButtonPress = () => {
     if(!gameStarted){
